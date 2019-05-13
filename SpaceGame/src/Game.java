abf36fac4b1e303ffe18948f08d2723d25037ec6
irazsanlii <50521068+irazsanlii@users.noreply.github.com>
2019-05-13 02:11:48 +0200

    Add files via upload

--- SpaceGame/src/Game.java
+++ SpaceGame/src/Game.java
@@ -18,19 +18,13 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
- * @author irasan19
+ * @author irazsanlii
  */
 
-class Firing {
-	// At every time that the actionPerformed method runs, the fire will try to go forward.
+class Firing { // We create an array to hold on to the fire. At every time that the actionPerformed method runs, the fire will try to go forward.
+	
 	private int x;
 	private int y;
 	
@@ -58,25 +52,25 @@ class Firing {
 
 public class Game extends JPanel implements KeyListener,ActionListener {
 
-    Timer timer=new Timer(5, this);
+    Timer timer=new Timer(5, this); // Set a timer to run the actionPerformed method.
     
     private int time=0; // The passing time.
     private int numberOfShots=0;
     
-    private BufferedImage img;
+    private BufferedImage img; 
     
     private ArrayList<Firing> fire=new ArrayList<>(); //  Hold on to the fire and provide to shoot more. 
     
     private int firedirY=5; // It will be added on the coordinate Y to provide movement of the fire at every time that the actionPerformed method runs.  
     private int ballX=0; // Move left and right.
-    private int balldirX=5; // If I do not give a first value, it can not move. It will be added on the ballX to provide the returning from right to left after meet with the limitations.	
-    private int spaceshipX=0; // Location of the spaceship.
+    private int balldirX=5; // It will run in the actionPerformed method, it will be added on the ballX to provide provide the returning from right to left after meet with the limitations. So, if I do not give a first value, ballX will not be change. It means there will not be any movement. 
+    private int spaceshipX=0; // First location of the spaceship.
     private int dirSpaceX=20; // According to the keyPressed method, advance 20 units to right or left.
       
     public Game() {       
     
         try {
-            img=ImageIO.read(new FileImageInputStream(new File("spaceship.png")));
+            img=ImageIO.read(new FileImageInputStream(new File("spaceship.png"))); // Load the image of spaceship.
         } catch (IOException ex) {
             Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -89,25 +83,26 @@ public class Game extends JPanel implements KeyListener,ActionListener {
     
         super.paint(g); 
         
-        time+=5;
+        time+=5; // To print the passing time when the game finish. Timer runs each 5 milliseconds that is why +=5. 
         
-        g.setColor(Color.RED);
+        g.setColor(Color.RED); // Create the ball.
         
         g.fillOval(ballX,0,20,20); 
         g.drawImage(img,spaceshipX,475,img.getWidth()/2,img.getHeight()/2,this); // Add the spaceship.
         
+        // At the following part, I want to remove the fires which leave from the frame not to slow down the program. But console's output:
         /*fire.stream().filter((firing) -> (firing.getY()<=0)).forEach((firing) -> {
             fire.remove(firing);
         });
         Exception in thread "AWT-EventQueue-0" java.lang.NullPointerException
 	at Game.lambda$paint$0(Game.java:99)*/
         
-        g.setColor(Color.ORANGE);
+        g.setColor(Color.ORANGE); // Draw the shots.
         fire.stream().forEach((firing) -> {
             g.fillRect(firing.getX(),firing.getY(),5,10);
         });
         
-        if (control()) {
+        if (control()) { // When the fire hits the ball, finish the game.
             timer.stop();
             String msg=
                     "You won!\n"+
@@ -125,7 +120,7 @@ public class Game extends JPanel implements KeyListener,ActionListener {
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e) { // Here we have codes to move the ball. This method will run at every time that the timer runs. 
         
         fire.stream().forEach((firing) -> {
             firing.setY(firing.getY()-firedirY);
@@ -140,29 +135,29 @@ public class Game extends JPanel implements KeyListener,ActionListener {
         if (ballX<=0) { // When the ball come back to the left side, block to go outside from the frame's left.
                 balldirX=-balldirX; 
         }
-        repaint(); // Show the ball's movement.   
+        repaint(); // Show the ball's movement. It will call the paint method.  
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         
-        int c=e.getKeyCode();
+        int c=e.getKeyCode(); // When we click, the keyCode will rotate a value according to the right or left clicking.
         
-        if (c==KeyEvent.VK_LEFT) {
+        if (c==KeyEvent.VK_LEFT) { // Check the click, if it is to the left.
             
-            if (spaceshipX<=0) { 
+            if (spaceshipX<=0) { // Check the location of the spaceship not to go out from the frame when we click.
                 spaceshipX=0;
             } else {
-                spaceshipX-=dirSpaceX;                
+                spaceshipX-=dirSpaceX; // Move the ship 20 units to the left.               
             }    
         }
         
         if (c==KeyEvent.VK_RIGHT) {
             
-            if (spaceshipX>=710) { 
+            if (spaceshipX>=710) { // Check the location of the spaceship not to go out from the frame when we click.
                 spaceshipX=710;
             } else {
-                spaceshipX+=dirSpaceX;                
+                spaceshipX+=dirSpaceX; // Move the ship 20 units to the right.               
             }    
         }   
         
